dab095101d47afb9aa7a2106188210d4fb4d701b
Bibhuti Bhusan Panda <emailofpanda@yahoo.com>
2017-09-14 00:42:47 +0530

    MainMenu.java documented(1.0)

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,7 +1,6 @@
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
+
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
@@ -13,95 +12,162 @@ import javax.swing.JLabel;
 
 public class MainMenu {
 
-    JFrame Menu = new JFrame("Maze");
-    JButton Start = new JButton("Play");
-    JButton Exit = new JButton("Exit");
-    JButton MapMaker = new JButton("Map Maker");
-    ImageIcon picture = new ImageIcon("res/Images/MazePicture.png");
-    JLabel imageLabel = new JLabel(picture);
-    ArrayList<String> mapList = new ArrayList<String>();
-    JComboBox<String> lvlList;
-    int menuWidth = 100; //Width of each button/item on display
-    int menuHeight = 30;//Height of each button/item on display
-    int menuY = 460; //Button/item location on display
-    int WIDTH = 490;
-    int HEIGHT = 530;
+    private JFrame Menu;//Main frame of the program
+    private JButton Start;//Variable for the Start button
+    private JButton Exit;//Variable for the Exit button
+    private JButton MapMaker;//Variable for the MapMaker button
+    private ImageIcon picture;//Variable for the picture to be retrieved
+    private JLabel imageLabel;//Variable to display the retrieved picture
+
+    private ArrayList<String> mapList;//Stores the names of the list of maps we have created or will create in future
+    private JComboBox<String> lvlList;//It contains the names of all the existing maps in it
+
+    private int menuWidth;//Width of each button/item on display
+    private int menuHeight;//Height of each button/item on display
+    private int menuY; //Button/item location on display
+    private int WIDTH;//Width of the menu
+    private int HEIGHT;//Height of the menu
     
             
 	public MainMenu() {
-    	//Load map list
+	    //This function initializes all the private variables hence increasing encapsulation
+        initialize();
+
+    	//This function loads the names of maps present in mapList variable
     	getMapList();
-    	lvlList = new JComboBox<String>(mapList.toArray(new String[mapList.size()]));
-    	
-        //Menu Variables
-        Menu.setResizable(false);
-        Menu.setSize(WIDTH, HEIGHT);
-        Menu.setLayout(null);
-        Menu.setLocationRelativeTo(null);
-        Menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        //Start Button Variables
-        Start.setSize(menuWidth,menuHeight);
-        Start.setLocation(10, menuY);
-        Menu.add(Start);
-        Start.addActionListener(new ActionListener(){
 
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				new Maze(lvlList.getSelectedItem().toString());
-				Menu.setVisible(false);
-			}
-        	
-        });	
-        
-        //Map Maker Button Variables
-        MapMaker.setSize(menuWidth,menuHeight);
-        MapMaker.setLocation(120, menuY);
-        Menu.add(MapMaker);
-        MapMaker.addActionListener(new ActionListener(){
+        //This method initializes the menu
+        setMenu();
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				new MazeMapMaker();
-				Menu.setVisible(false);
-			}
-        	
-        });
+        //This method sets the necessary functionality of the Start button
+        setStartButton();
         
-        //Level Selector
-        lvlList.setSize(menuWidth+35, menuHeight);
-        lvlList.setLocation(230, menuY);
-        Menu.add(lvlList);
+        //This method sets the necessary functionality of the MapMaker button
+        setMapMakerButton();
         
-        //Exit Button Variables
-        Exit.setSize(menuWidth,menuHeight);
-        Exit.setLocation(375,menuY);
-        Menu.add(Exit);
-        Exit.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-	            System.exit(0);
-			}
-        });
+        //This method initializes the level selector drop down list
+        setLevelSelectorDropDown();
+        
+        //This method sets the necessary functionality of the exit button
+        setExitButton();
         
         //Display Picture
+        setDisplayPicture();
+    }
+
+    /**
+     * This method initializes all the necessary private
+     * variables which are to be used in future
+     */
+    private void initialize(){
+        this.Menu = new JFrame("Maze");//Sets the title of the menu bar as "Maze"
+        this.Start = new JButton("Play");//Sets the title of the button as "Play"
+        this.Exit = new JButton("Exit");//Sets the title of another button as "Exit"
+        this.MapMaker = new JButton("Map Maker");//Sets the title fo another button as "Map Maker"
+        this.picture = new ImageIcon("res/Images/MazePicture.png");//Loads the picture from a given location to the variable
+        this.imageLabel = new JLabel(picture);//Loads the picture from the variable to the label in the centre position
+
+        this.menuWidth = 100;
+        this.menuHeight = 30;
+        this.menuY = 460;
+        this.WIDTH = 550;
+        this.HEIGHT = 530;
+    }
+
+    /**
+     * This method is explicitly there to check whether the file we are trying to
+     * get from outside exists or not. If the file exists then it populates the file
+     * name in mapList variable
+     */
+    private void getMapList(){
+        this.mapList = new ArrayList<>();//initializes the mapList are makes it ready to store the name of maps present
+
+        //gets the file and loads it into mapList if the file exists
+    	for(int i = 0; i < 99; i++){
+    		File map = new File("./Level "+i+".map");//tries to retrieve file from the source
+    		if(map.exists()){//checks for whether the file exists or not
+    			System.out.println("Level "+i+" exists");//for debugging purpose as it displays the file which is added
+    			mapList.add("Level "+i+".map");//adds the name of the file list to the mapList
+    		}
+    	}
+
+    }
+
+    /**
+     * This method deals with housekeeping stuffs of the menu
+     */
+    private void setMenu(){
+        Menu.setResizable(false);//menu is static
+        Menu.setSize(WIDTH, HEIGHT);//sets the initial size(or size) of the menu
+        Menu.setLayout(null);//sets the layout
+        Menu.setLocationRelativeTo(null);//default pop-up, no need to think about any relative location to pop up
+        Menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//enables the default close button i.e. `x` button
+    }
+
+    /**
+     * This method sets all the necessary functionality of the
+     * Start button
+     */
+    private void setStartButton(){
+        Start.setSize(menuWidth,menuHeight);//sets size of the button
+        Start.setLocation(10, menuY);//sets the location of the button
+        Menu.add(Start);//adds the button to main frame
+        //function to define what happens when you click the button
+        Start.addActionListener(event->{
+            /*
+             * When the start button is clicked extract the name of the selected maze
+             * and navigate to the extracted maze.
+             */
+            new Maze(lvlList.getSelectedItem().toString());
+            Menu.setVisible(false);//after we navigate to the required map hide the main menu
+        });
+    }
+
+    /**
+     * This method sets all the necessary functionality of the
+     * MapMaker button
+     */
+    private void setMapMakerButton(){
+        MapMaker.setSize(menuWidth+15,menuHeight);//sets the size of the button
+        MapMaker.setLocation(122, menuY);//sets the button at the specified location
+        Menu.add(MapMaker);//add the button to the menu
+        //function to define what happens when you click the button
+        MapMaker.addActionListener(event->{
+            /*
+             * It either creates a new map or edits the existing map
+             */
+            new MazeMapMaker();
+            Menu.setVisible(false);//after we navigate to the required location, make the menu invisible
+        });
+    }
+
+    /**
+     * This method initializes the drop down list
+     */
+    private void setLevelSelectorDropDown(){
+        lvlList = new JComboBox<>(mapList.toArray(new String[mapList.size()]));//put all the map names into it
+        lvlList.setSize(menuWidth+35, menuHeight);//sets the size of the drop down
+        lvlList.setLocation(250, menuY);//put the dropdown in the specified location
+        Menu.add(lvlList);//add the drop down to the main menu
+    }
+
+    /**
+     * This method initializes and sets all the necessary functionality of the exit button
+     */
+    private void setExitButton(){
+        Exit.setSize(menuWidth,menuHeight);//sets the size of the exit button
+        Exit.setLocation(397,menuY);//sets the location where the button to be put
+        Menu.add(Exit);//adds exit to the main menu
+        Exit.addActionListener(event->System.exit(0));//function to execute when the button is pressed
+    }
+
+    /**
+     * This method initializes the picture taken from the source to the main menu
+     */
+    private void setDisplayPicture(){
         imageLabel.setBounds((WIDTH-412)/2, 25, 412, 412);
         imageLabel.setVisible(true);
         Menu.add(imageLabel);
         Menu.setVisible(true);
-    }
-
-    static boolean levelsExistAlready = false;
-
-    public void getMapList(){
-    	for(int i = 0; i < 99; i++){
-    		File map = new File("./Level "+i+".map");
-    		if(map.exists()){
-    			System.out.println("Level "+i+" exists");
-    			mapList.add("Level "+i+".map");
-    			levelsExistAlready = true;
-    		}
-    	}
     }
 }
